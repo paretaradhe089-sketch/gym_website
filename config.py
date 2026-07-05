37f68f6f28de5a4aa829a62a046d2e589a280da7
paretaradhe089-sketch <paretaradhe089@gmail.com>
2026-07-05 16:08:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-# config.py
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-spartan-key-2024')
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/spartanDB')
-ADMIN_PASSWORD = "gym@57675656"
+ADMIN_PASSWORD = "gym57675656"
+
+MAIL_EMAIL = os.environ.get('MAIL_EMAIL', 'your_email@gmail.com')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_app_password')
+UPI_ID = "sen.ankur1@ybl"
