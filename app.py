1bced31e4392eca9928cebb739d48d8e3181e214
paretaradhe089-sketch <paretaradhe089@gmail.com>
2026-07-05 16:06:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask
 from config import SECRET_KEY
 from models.database import init_db
@@ -14,5 +13,4 @@ app.register_blueprint(main_bp)
 app.register_blueprint(admin_bp)
 
 if __name__ == '__main__':
-    print("🏋️ Spartan Fitness Zone Starting on http://localhost:5000")
     app.run(debug=True, port=5000)
