1a23cfba91a42cb6d38b6e05d66a467e26a72e31
paretaradhe089-sketch <paretaradhe089@gmail.com>
2026-07-05 18:07:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,15 @@
+# import os
+
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-spartan-key-2024')
+# MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/spartanDB')
+# ADMIN_PASSWORD = "gym57675656"
+
+# MAIL_EMAIL = os.environ.get('MAIL_EMAIL', 'your_email@gmail.com')
+# MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_app_password')
+# UPI_ID = "sen.ankur1@ybl"
+
+
+
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-spartan-key-2024')
@@ -7,3 +19,6 @@ ADMIN_PASSWORD = "gym57675656"
 MAIL_EMAIL = os.environ.get('MAIL_EMAIL', 'your_email@gmail.com')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_app_password')
 UPI_ID = "sen.ankur1@ybl"
+
+# Admin ka WhatsApp Number (Country code 91 ke sath bina + ke)
+ADMIN_WHATSAPP = "918440088706" 
