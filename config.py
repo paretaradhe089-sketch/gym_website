f5e9f7aa8fba24de2c476a8bca055861ad165192
paretaradhe089-sketch <paretaradhe089@gmail.com>
2026-07-06 21:06:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,15 +10,38 @@
 
 
 
+# import os
+
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-spartan-key-2024')
+# MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/spartanDB')
+# ADMIN_PASSWORD = "gym57675656"
+
+# MAIL_EMAIL = os.environ.get('MAIL_EMAIL', 'your_email@gmail.com')
+# MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_app_password')
+# UPI_ID = "sen.ankur1@ybl"
+
+# # Admin ka WhatsApp Number (Country code 91 ke sath bina + ke)
+# ADMIN_WHATSAPP = "918440088706" 
+
+
+
+
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-spartan-key-2024')
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/spartanDB')
 ADMIN_PASSWORD = "gym57675656"
 
+# Razorpay Keys (Render par Environment Variables mein daalna hoga)
+RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID', 'rzp_test_XXXXXXXXXXXX')
+RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET', 'XXXXXXXXXXXXXXXXXXXX')
+
 MAIL_EMAIL = os.environ.get('MAIL_EMAIL', 'your_email@gmail.com')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_app_password')
 UPI_ID = "sen.ankur1@ybl"
+ADMIN_WHATSAPP = "918440088703" 
 
-# Admin ka WhatsApp Number (Country code 91 ke sath bina + ke)
-ADMIN_WHATSAPP = "918440088706" 
+# Contact Details
+GYM_PHONE = "8440088703"
+GYM_WHATSAPP = "https://wa.me/918440088703"
+GYM_INSTAGRAM = "https://www.instagram.com/sfz_jaipur?igsh=MTRvZnljMndzdDNyag=="
