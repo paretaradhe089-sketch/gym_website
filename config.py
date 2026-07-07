dd0dbca5285087c8cf18f402fa6bdf900960671f
paretaradhe089-sketch <paretaradhe089@gmail.com>
2026-07-07 14:35:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # import os
 
 # SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-spartan-key-2024')
@@ -27,30 +26,25 @@
 
 
 
-=======
->>>>>>> dfea781 (Added Hero Banner, Fixed Razorpay and Group Image)
+
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-spartan-key-2024')
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/spartanDB')
 ADMIN_PASSWORD = "gym57675656"
 
-<<<<<<< HEAD
-# Razorpay Keys (Render par Environment Variables mein daalna hoga)
+# Razorpay Keys (Render पर Environment Variables में सेट करें)
 RAZORPAY_KEY_ID = os.environ.get('RAZORPAY_KEY_ID', 'rzp_test_XXXXXXXXXXXX')
 RAZORPAY_KEY_SECRET = os.environ.get('RAZORPAY_KEY_SECRET', 'XXXXXXXXXXXXXXXXXXXX')
 
 MAIL_EMAIL = os.environ.get('MAIL_EMAIL', 'your_email@gmail.com')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_app_password')
+
 UPI_ID = "sen.ankur1@ybl"
-ADMIN_WHATSAPP = "918440088703" 
+
+ADMIN_WHATSAPP = "918440088703"
 
 # Contact Details
 GYM_PHONE = "8440088703"
 GYM_WHATSAPP = "https://wa.me/918440088703"
 GYM_INSTAGRAM = "https://www.instagram.com/sfz_jaipur?igsh=MTRvZnljMndzdDNyag=="
-=======
-MAIL_EMAIL = os.environ.get('MAIL_EMAIL', 'your_email@gmail.com')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'your_app_password')
-UPI_ID = "sen.ankur1@ybl"
->>>>>>> dfea781 (Added Hero Banner, Fixed Razorpay and Group Image)
